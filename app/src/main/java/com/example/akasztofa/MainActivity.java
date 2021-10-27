60a98c882b2889d0e0f2dcc67ca79b99dceb8eb9
Poós Dániel <poosdani@gmail.com>
2021-10-27 21:24:09 +0200

    ...

--- app/src/main/java/com/example/akasztofa/MainActivity.java
+++ app/src/main/java/com/example/akasztofa/MainActivity.java
@@ -8,15 +8,19 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Random;
+import java.util.RandomAccess;
+
 public class MainActivity extends AppCompatActivity {
     private Button plus, minus, guessButton;
     private TextView guessTV, solutionTV;
     private ImageView hangman;
+    private Random r;
     private char guessedChar;
     private char[] abc; //35
     private int guessCounter;
     private String[] words = {"január", "február", "március", "április", "május", "június", "július", "augusztus", "szeptember", "október", "november", "december"};
-
+    private String randomWord;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,9 +48,14 @@ public class MainActivity extends AppCompatActivity {
         solutionTV = findViewById(R.id.solution);
         hangman = findViewById(R.id.picture);
         abc = "aábcdeéfghiíjklmnoóöőpqrstuúüűvwxyz".toUpperCase().toCharArray();
-
+        randomWord = words[r.nextInt()*12];
     }
-    public void setSolutionTV(char guessed){
-
+    public void setBasicSolutionTV(){
+        int numberOfChar = randomWord.length();
+        String s = "";
+        for (int i = 0; i < numberOfChar; i++) {
+            s += "_ ";
+        }
+        solutionTV.setText(s);
     }
 }
