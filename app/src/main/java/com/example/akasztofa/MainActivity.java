f590be50171ef00326232ad7f4fe566e94a49610
Poós Dániel <poosdani@gmail.com>
2021-10-30 23:47:56 +0200

    overthinking 100

--- app/src/main/java/com/example/akasztofa/MainActivity.java
+++ app/src/main/java/com/example/akasztofa/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.akasztofa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,11 +16,14 @@ public class MainActivity extends AppCompatActivity {
     private Button plus, minus, guessButton;
     private TextView guessTV, solutionTV;
     private ImageView hangman;
-    private Random r;
-    private char[] abc; //35
+    private char[] abc;
+    private String used = "";
     private int guessCounter;
     private final String[] words = {"január", "február", "március", "április", "május", "június", "július", "augusztus", "szeptember", "október", "november", "december"};
     private String randomWord;
+    private boolean correct;
+    private String answer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,12 +31,12 @@ public class MainActivity extends AppCompatActivity {
         init();
         plus.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                guessTV.setText(charPlus());
+                guessTV.setText(charPlus()+"");
             }
         });
         minus.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                guessTV.setText(charMinus());
+                guessTV.setText(charMinus()+"");
             }
         });
         guessButton.setOnClickListener(new View.OnClickListener() {
@@ -49,13 +54,20 @@ public class MainActivity extends AppCompatActivity {
         solutionTV = findViewById(R.id.solution);
         hangman = findViewById(R.id.picture);
         abc = "aábcdeéfghiíjklmnoóöőpqrstuúüűvwxyz".toUpperCase().toCharArray();
-        randomWord = words[r.nextInt()*12];
         guessCounter =0;
+        setBasicSolutionTV();
     }
     public void setBasicSolutionTV(){
+        correct = false;
+        randomWord = words[(int)(Math.random()*12)];
+        randomWord = randomWord.toUpperCase();
         int numberOfChar = randomWord.length();
         String s = "";
-        for (int i = 0; i < numberOfChar; i++) s += "_ ";
+        for (int i = 0; i < numberOfChar; i++) {
+            s += "_ ";
+            answer += randomWord.toCharArray()[i]+" ";
+        }
+        solutionTV.setTextColor(Color.RED);
         solutionTV.setText(s);
     }
     public char charPlus(){
@@ -65,6 +77,8 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < abc.length; i++) {
             if(currentChar == abc[i]) index = i;
         }
+        if (!(used.contains(currentChar+""))) guessTV.setTextColor(Color.RED);
+        else guessTV.setTextColor(Color.BLACK);
         return abc[index+1];
     }
     public char charMinus(){
@@ -74,13 +88,16 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < abc.length; i++) {
             if(currentChar == abc[i]) index = i;
         }
+        if (!(used.contains(currentChar+""))) guessTV.setTextColor(Color.RED);
+        else guessTV.setTextColor(Color.BLACK);
         return abc[index-1];
     }
     public void guessButtonPressed(){
         String currentString = guessTV.getText().toString();
-        String solChang = solutionTV.getText().toString();
-        if (!(randomWord.toUpperCase().contains(currentString))) {
-            //Toast.makeText(this, "Nincs benne illyen bettű ebben a szóban", Toast.LENGTH_SHORT);
+        String solutionString = solutionTV.getText().toString();
+        if (!(randomWord.contains(currentString))) {
+            used += currentString;
+            guessTV.setTextColor(Color.BLACK);
             guessCounter++;
             switch (guessCounter){
                 case 1: hangman.setImageResource(R.drawable.akasztofa01);break;
@@ -95,21 +112,39 @@ public class MainActivity extends AppCompatActivity {
                 case 10: hangman.setImageResource(R.drawable.akasztofa10);break;
                 case 11: hangman.setImageResource(R.drawable.akasztofa11);break;
                 case 12: hangman.setImageResource(R.drawable.akasztofa12);break;
-                case 13:
-                    hangman.setImageResource(R.drawable.akasztofa13);
-                    //TODO: Alertdialopg
-                break;
+                case 13: hangman.setImageResource(R.drawable.akasztofa13);break;
                 default:break;
             }
         }
         else{
+            used += currentString;
+            guessTV.setTextColor(Color.BLACK);
             for (int i= 0; i < randomWord.toCharArray().length; i++){
                 if (randomWord.toCharArray()[i] == currentString.toCharArray()[0]){
                     solutionTV.setText(currentString.toCharArray(),i*2, 1);
                 }
             }
+            solutionTV.setText(solutionString);
+            if (answer.equals(solutionTV.getText())) correct = true;
         }
-        solutionTV.setText(solChang);
+
+        if(guessCounter == 13 || correct) {
+            AlertDialog.Builder b = new AlertDialog.Builder(MainActivity.this);
+            if (guessCounter == 13) b.setTitle("Vereség");
+            else b.setTitle("Győzelem");
+            b.setMessage("Vége a játéknak. Szeretne új játékot kezdeni?");
+            b.setPositiveButton("Yes", (dialog, which) -> {
+                guessCounter = 0;
+                setBasicSolutionTV();
+                hangman.setImageResource(R.drawable.akasztofa00);
+            });
+            b.setNegativeButton("No", (dialog, which) -> {
+                    finishAffinity();
+            });
+            b.setCancelable(false);
+            AlertDialog dialog = b.create();
+            dialog.show();
+        }
     }
 
 }
